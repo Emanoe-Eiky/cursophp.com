af6feaa16dc022ad2d5a14bda0fff569b45e7a25
Emanoel Eike Ferreira da Silva <eike.em.2017@gmail.com>
2021-01-15 13:44:26 -0300

    validaçao de fórmulario

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="Width=device-width, initial-scale=1">
+    <link rel= "stylesheet" href="estilo.css">
+    <title>FORMULARIO</title>
+</head>
+<body>
+  <div class="titulo">
+    <div class="">
+    <br/>
+    <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+    </div> <br>
+<form action="script.php" method="post">
+
+    <div>
+    <p>Seu nome <input type="text" name="nome"/></p>
+    </div><br/>
+
+    <div>
+    <p>Sua idade <input type="text" name="idade"/></p>
+    </div><br/>
+
+    <div>
+    <p> <input type="submit" value="Envie seus dados"/></p>
+    </div>
+   
+
+</form>
+</div>
+</body>
+</html>
